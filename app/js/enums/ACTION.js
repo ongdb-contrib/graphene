c9f201e2b8189a109deee462b1a864a76ccd2771
crazyyanchao <yanchaoma@foxmail.com>
2022-02-13 04:06:20 +0000

    Deploying to main from  @ 0e0168dc7ddb1f280d07da5e0cb3ef9b938899a7 🚀

--- app/js/enums/ACTION.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-export default {
-  CREATE_NODE: 'CREATE_NODE',
-  CREATE_EDGE: 'CREATE_EDGE',
-  DELETE_NODE: 'DELETE_NODE',
-  DELETE_EDGE: 'DELETE_EDGE'
-};
--- app/js/utils/graphql/shared/tools.js
+++
@@ -1,3 +0,0 @@
-export default {
-  getName: (edge, suffix) => `${edge.startNode.label.toCamelCase()}${edge.label.toCamelCase()}${edge.endNode.label.toCamelCase()}${suffix}`
-}
