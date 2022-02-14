6a26b20069287fe7a1f9694a0269781798d9fba9
YanchaoMa <yanchaoma@foxmail.com>
2022-02-14 21:49:48 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
   },
   output: {
     path: path.join(__dirname, 'build'),
-    publicPath: process.env.GITHUB_PAGES ? `/${process.env.GITHUB_PAGES}/` : '/';,
+    publicPath: '/graphene/';,
     filename: 'bundle.js'
   },
   plugins: [
